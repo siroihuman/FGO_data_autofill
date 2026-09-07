45855bcaa53afc90a13ff522f7f194befcf40d7f
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-07 23:02:51 +0900

    Load ver 2.6.0 variant support

--- FGO_DataAutofill_atwiki.js
+++ FGO_DataAutofill_atwiki.js
@@ -4,7 +4,7 @@
   const current = document.currentScript;
   const source = current && current.src ? current.src : '';
   const base = source ? source.replace(/[^/?#]+(?:[?#].*)?$/, '') : '';
-  const VERSION = '2.5.0';
+  const VERSION = '2.6.0';
   const files = [
     'FGO_DataAutofill_core.js',
     'FGO_DataAutofill_runtime_patch.js',
