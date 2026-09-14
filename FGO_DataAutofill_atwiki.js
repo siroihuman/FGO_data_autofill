1532efb0bccdbf9ef0d3834f67dbeed6fa09bbf7
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-14 16:20:06 +0900

    chore: load ver 2.6.4 icon picker

--- FGO_DataAutofill_atwiki.js
+++ FGO_DataAutofill_atwiki.js
@@ -4,15 +4,17 @@
   const current = document.currentScript;
   const source = current && current.src ? current.src : '';
   const base = source ? source.replace(/[^/?#]+(?:[?#].*)?$/, '') : '';
-  const VERSION = '2.6.3';
+  const VERSION = '2.6.4';
   const files = [
     'FGO_DataAutofill_core.js',
     'FGO_DataAutofill_runtime_patch.js',
     'FGO_DataAutofill_ascension_patch.js',
     'FGO_DataAutofill_np_stage_guard.js',
     'FGO_DataAutofill_ascension_span_patch.js',
+    'FGO_DataAutofill_gender_patch.js',
     'FGO_DataAutofill_ui.js',
     'FGO_DataAutofill_ascension_ui_patch.js',
+    'FGO_DataAutofill_icon_picker_patch.js',
     'FGO_DataAutofill_boot.js'
   ];
   let sequence = Promise.resolve();
