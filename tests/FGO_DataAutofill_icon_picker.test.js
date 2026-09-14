b6e4dbdf5384a709da24524ccac52922c9d0b371
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-15 00:23:29 +0900

    test: cover bond CE effect icon popup picker

--- tests/FGO_DataAutofill_icon_picker.test.js
+++ tests/FGO_DataAutofill_icon_picker.test.js
@@ -10,15 +10,17 @@ require('../FGO_DataAutofill_gender_patch.js');
 require('../FGO_DataAutofill_ui.js');
 require('../FGO_DataAutofill_ascension_ui_patch.js');
 require('../FGO_DataAutofill_icon_picker_patch.js');
+require('../FGO_DataAutofill_bond_icon_picker_patch.js');
 require('../FGO_DataAutofill_icon_refresh_patch.js');
 
 const core = global.FGODataAutofillCore;
 const ui = global.FGODataAutofillUI;
-assert.strictEqual(core.VERSION, '2.6.6');
+assert.strictEqual(core.VERSION, '2.6.7');
 assert.strictEqual(ui.skillIconCatalogPageUrl, 'https://w.atwiki.jp/siroi_human/pages/20.html');
 assert.strictEqual(typeof ui.skillIconPopupHtml, 'function');
 assert.strictEqual(typeof ui.refreshSkillIconCatalog, 'function');
 assert.strictEqual(typeof ui.skillIconRefreshButtonHtml, 'function');
+assert.strictEqual(ui.bondCraftEssenceIconPickerEnabled, true);
 assert(ui.skillIconRefreshButtonHtml().includes('data-skill-icon-refresh'));
 assert(ui.skillIconRefreshButtonHtml().includes('アイコン一覧を更新'));
 
@@ -29,7 +31,7 @@ state.ownedSkills[0].ascensionMode = 'full';
 state.ownedSkills[0].ascensionData.second.icon = '防御力アップ.png';
 state.ownedSkills[0].special.enabled = true;
 state.ownedSkills[0].special.data.icon = 'NP増加.png';
-state.bondCraftEssence.icon = '礼装アイコン.png';
+state.bondCraftEssence.icon = '礼装効果.png';
 
 const root = { innerHTML: '' };
 ui.render(root, state);
@@ -43,8 +45,10 @@ assert(root.innerHTML.includes('data-skill-icon-open'));
 assert(root.innerHTML.includes('アイコンを選択'));
 assert(!root.innerHTML.includes('data-skill-icon-catalog'));
 assert(!root.innerHTML.includes('アイコン名を検索'));
-assert(root.innerHTML.includes('data-path="bondCraftEssence.icon"'));
-assert(!root.innerHTML.includes('data-skill-icon-picker-for="bondCraftEssence.icon"'));
+assert(root.innerHTML.includes('data-skill-icon-picker-for="bondCraftEssence.icon"'));
+assert(root.innerHTML.includes('type="hidden" data-path="bondCraftEssence.icon" value="礼装効果.png"'));
+assert(root.innerHTML.includes('<span>効果アイコン</span>'));
+assert(!root.innerHTML.includes('<span>礼装アイコン</span><input data-path="bondCraftEssence.icon"'));
 
 const popup = ui.skillIconPopupHtml();
 assert(popup.includes('data-skill-icon-modal'));
@@ -58,4 +62,4 @@ assert.strictEqual(core.normalizeState(state).basic.gender, '性別不明');
 const page = core.buildFreshPage(state);
 assert(page.includes(':性別|-|'));
 
-console.log('FGO_DataAutofill icon popup / refresh / unknown gender tests passed');
+console.log('FGO_DataAutofill icon popup / bond CE / refresh / unknown gender tests passed');
