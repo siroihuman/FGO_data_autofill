59e2434ea32f4c3ecc2a20c0fbf4b5d52ce2c65c
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-15 16:31:01 +0900

    test: expect v2.6.13 basic profile pipeline

--- tests/FGO_DataAutofill_icon_picker.test.js
+++ tests/FGO_DataAutofill_icon_picker.test.js
@@ -17,7 +17,7 @@ require('../FGO_DataAutofill_basic_profiles_output_guard.js');
 
 const core = global.FGODataAutofillCore;
 const ui = global.FGODataAutofillUI;
-assert.strictEqual(core.VERSION, '2.6.9');
+assert.strictEqual(core.VERSION, '2.6.13');
 assert.strictEqual(ui.skillIconCatalogPageUrl, 'https://w.atwiki.jp/siroi_human/pages/20.html');
 assert.strictEqual(typeof ui.skillIconPopupHtml, 'function');
 assert.strictEqual(typeof ui.refreshSkillIconCatalog, 'function');
