0d57ea94d4d813231ab79599adb0277608be0ebe
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-15 16:02:42 +0900

    chore: mark basic profile sync as v2.6.11

--- FGO_DataAutofill_basic_profiles_dom_sync.js
+++ FGO_DataAutofill_basic_profiles_dom_sync.js
@@ -5,7 +5,7 @@
   const ui = globalThis.FGODataAutofillUI;
   if (!core || !ui) throw new Error('FGO Data Autofill modules are not loaded.');
 
-  const VERSION = '2.6.10';
+  const VERSION = '2.6.11';
 
   function blankProfile() {
     return { gender: '', height: '', weight: '', note: '' };
